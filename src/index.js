0505dee60e9a4aa56585e2f55b4414518fc04241
Myrkvithr <place_of_meeting@inbox.ru>
2022-01-06 22:04:10 +0300

    feat: added a solution to the task to index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const str = Math.abs(n) + '';
+    console.log(str);
+    const chengedNum = str.split('').reverse().join('');
+    return Math.abs(Number(chengedNum));
 }
